f31a9d1df7fe87382ef29489b394a073c82e5e8a
Yeats <wstgzy@126.com>
2019-09-04 14:36:12 +0800

    yml配置文件

--- src/main/java/cn/yeats/controller/HelloController.java
+++ src/main/java/cn/yeats/controller/HelloController.java
@@ -1,6 +1,7 @@
 package cn.yeats.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,20 @@ public class HelloController {
     @Autowired
     private DataSource dataSource;
 
+    //----读取yml配置文件中的内容----
+    @Value("${baidu.url}")
+    private String baiduUrl;
+    @Value("${google.url}")
+    private String googleUrl;
+
     @GetMapping("/hello")   // 请求路径：http://localhost:8080/hello
     public String hello() {
         // 打印数据源到控制，打个断点，观察一下
         System.out.println("dataSource" + dataSource);
+
+        // 多个yml配置文件的测试
+        System.out.println(baiduUrl);
+        System.out.println(googleUrl);
 
         return "Hello, Spring Boot.";
     }
