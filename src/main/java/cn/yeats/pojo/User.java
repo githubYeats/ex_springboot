dd83840dba9b6744ea120de360d9a08cf8bba5aa
Yeats <wstgzy@126.com>
2019-11-11 01:48:55 +0800

    修改数据库，使用腾讯云服务器

--- src/main/java/cn/yeats/pojo/User.java
+++ src/main/java/cn/yeats/pojo/User.java
@@ -13,19 +13,25 @@ import java.util.Date;
 @Data // lombok注解，自动提供getter和setter、hashCode、equals、toString等方法
 public class User implements Serializable {
     @Id
-    //开启主键自动回填
-    @KeySql(useGeneratedKeys = true)
+    @KeySql(useGeneratedKeys = true)//开启主键自动回填
     private Integer id; // ID
 
     //@Column(name = "abc") // 将表中abc字段映射到pojo的username属性
     private String username; // 用户名
 
     private String password; // 密码
+
     private String name; // 真实姓名
+
     private Integer age; // 年龄
+
     private Integer sex; // 性别：1男，2女
+
     private String birthday; // 出生日期
+
     private String note; // 备注
+
     private Date created; // 创建时间
+
     private Date updated; // 更新时间
 }
